f8d66753f2afdf394e5773c7c6149e7e6cce4b56
brunotoresan <42551829+brunotoresan@users.noreply.github.com>
2019-04-14 23:47:15 -0300

    Update utils.c

--- cthread/src/utils.c
+++ cthread/src/utils.c
@@ -4,3 +4,27 @@
 #include "../include/support.h"
 #include "../include/cthread.h"
 
+//Declaração das 3 filas de aptos utilizada pelo escalonador: prioridades alta, média e baixa
+PFILA2 high_priority_queue;
+PFILA2 average_priority_queue;
+PFILA2 low_priority_queue;
+
+
+/*-----------------------------------------------------------------------------------------
+
+Função:	Inicializa as filas de aptos
+
+Ret:	==0, se conseguiu
+
+	!=0, caso contrário (erro ou fila vazia na inicialização de alguma das filas de apto)
+
+------------------------------------------------------------------------------------------*/
+int cinit_queues() {
+    int queues_creation_status = 0;
+
+    queues_creation_status += CreateFila2(high_priority_queue);
+    queues_creation_status += CreateFila2(average_priority_queue); 
+    queues_creation_status += CreateFila2(low_priority_queue); 
+
+    return queues_creation_status
+}
