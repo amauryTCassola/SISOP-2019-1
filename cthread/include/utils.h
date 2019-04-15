77d8695f3a374200412a1b4dd98e81b9869d9ebb
brunotoresan <42551829+brunotoresan@users.noreply.github.com>
2019-04-14 23:48:11 -0300

    Update utils.h

--- cthread/include/utils.h
+++ cthread/include/utils.h
@@ -1,6 +1,20 @@
 
 #ifndef __CUTILS_
+
 #define __CUTILS_
+
+
+/******************************************************************************
+Parâmetros:
+
+Retorno:
+
+	Quando executada corretamente: retorna 0 (zero)
+
+	Caso contrário, retorna um valor diferente de 0 (zero).
+
+******************************************************************************/
+int cinit_queues();
 
 
 #endif
