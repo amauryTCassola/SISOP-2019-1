43d4c8532fd4f77310c25c272bfd6681c9275ebb
Bruno Ramos Toresan <42551829+brunotoresan@users.noreply.github.com>
2019-04-19 14:42:54 -0300

    Update utils.c

--- cthread/src/utils.c
+++ cthread/src/utils.c
@@ -21,3 +21,53 @@ int cinit_queues() {
 
     return queues_creation_status
 }
+
+
+/******************************************************************************
+Parâmetros:
+Retorno:
+	Quando executada corretamente: retorna 0
+	Caso contrário, retorna -9
+******************************************************************************/
+int dispatcher_para_fila_de_aptos(TCB_t* thread_leaving_CPU, TCB_t* thread_arriving_CPU){
+
+    status = 0;
+
+    //salva o contexto da thread que está em execução e que vai perder a CPU
+    getcontext(&thread_leaving_CPU->context);
+
+    //põe a thread que vai entrar na CPU em execução
+    setcontext(&thread_arriving_CPU->context);
+
+    //coloca a thread que está saindo da execução na fila de aptos correspondente a sua execução
+    switch(&thread_leaving_CPU->prio){
+        case 0:
+            status += AppendFila2(high_priority_queue, *thread_leaving_CPU);
+            break
+        case 1:
+            status += AppendFila2(average_priority_queue, *thread_leaving_CPU);
+            break
+        case 2:
+            status += AppendFila2(low_priority_queue, *thread_leaving_CPU);
+            break
+        default:
+            return -9;
+    }
+
+    if(status != 0){
+        return -9;
+    }
+
+    return 0;    
+}
+
+
+/******************************************************************************
+Parâmetros:
+Retorno:
+	Quando executada corretamente: retorna 0
+	Caso contrário, retorna -9
+******************************************************************************/
+int escalonador(){
+    
+}
