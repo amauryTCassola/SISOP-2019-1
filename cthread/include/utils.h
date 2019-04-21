51c01b2e091528a4e366b4e2c5ef07aff0688fed
Bruno Ramos Toresan <42551829+brunotoresan@users.noreply.github.com>
2019-04-20 22:39:03 -0300

    Update utils.h

--- cthread/include/utils.h
+++ cthread/include/utils.h
@@ -10,61 +10,34 @@ PFILA2 high_priority_queue;
 PFILA2 average_priority_queue;
 PFILA2 low_priority_queue;
 
-//Declaração da thread que está em execução
+//Declaração da variável global que representa a thread que está em execução
 TCB_t thread_in_execution;
+
+//Declaração da variável global que representa a thread main
 TCB_t thread_main;
 
-/******************************************************************************
-Parâmetros:
-Retorno:
-	Quando executada corretamente: retorna CODIGO_SUCESSO
-	Caso contrário, retorna CODIGO_ERRO.
-******************************************************************************/
+/*-----------------------------------------------------------------------------------------
+Função:	Inicializa as filas de aptos
+Ret: ==0, se conseguiu
+	 !=0, caso contrário (erro ou fila vazia na inicialização de alguma das filas de apto)
+------------------------------------------------------------------------------------------*/
 int cinit_queues();
 
 
-/******************************************************************************
-Parâmetros:
-Retorno:
-	Quando executada corretamente: retorna CODIGO_SUCESSO
-	Caso contrário, retorna CODIGO_ERRO
-******************************************************************************/
-int dispatcher(TCB_t *thread_leaving_CPU, TCB_t *thread_arriving_CPU);
-
-
 
 /******************************************************************************
-Parâmetros:
+Parâmetros: thread que será inserida na fila de aptos
 Retorno:
 	Quando executada corretamente: retorna CODIGO_SUCESSO
-	Caso contrário, retorna CODIGO_ERRO
-******************************************************************************/
-int escalonador_cyield();
-
-
-/******************************************************************************
-Parâmetros:
-Retorno:
-	Quando executada corretamente: retorna CODIGO_SUCESSO
-	Caso contrário, retorna CODIGO_ERRO
-
-ATENÇÃO:
-Função para o David usar para inserir threads bloqueadas ou recem criadas
-na fila de aptos
+	Quando executada erroneamente: retorna CODIGO_ERRO
 ******************************************************************************/
 int insere_na_fila_de_aptos(TCB_t *thread);
 
 
 /***********************************************************************************************
 Parâmetros:
-Retorno:
-	Quando executada corretamente: retorna CODIGO_SUCESSO
-	Caso contrário, retorna CODIGO_ERRO
-
-ATENÇÃO:
-Função para ser chamada quando a thread em execução foi para o estado bloqueado, e então
-esse escalonador vai escolher a nova thread na fila de aptos a ser executada
+Retorno: Retorna uma variável do tipo TCB_t que é a thread que o escalonador selecionou
 *************************************************************************************************/
-int escalonador_cjoin();
+int escalonador();
 
 #endif
