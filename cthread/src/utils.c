e84d779fa6aafce8329b186d9956dd757980e951
Bruno Ramos Toresan <42551829+brunotoresan@users.noreply.github.com>
2019-04-24 21:11:17 -0300

    Update utils.c

--- cthread/src/utils.c
+++ cthread/src/utils.c
@@ -24,52 +24,28 @@ int cinit_queues() {
 }
 
 
-/*-----------------------------------------------------------------------------------------
-Função:	Implementação do chaveamento de contexto e do despachante (dispatcher)
-
-Parâmetros: 
-thread_leaving_cpu (ponteiro para a thread que está perdendo a CPU),
-new_state_thread_leaving_cpu (novo estado da thread que está perdendo a CPU: PROCST_APTO, PROCST_BLOQ, PROCST_TERMINO)
-
-Ret: void
-------------------------------------------------------------------------------------------*/
 void dispatcher(int new_state_thread_leaving_cpu){
 
-    TCB_t* thread_arriving_CPU = (TCB_t*)malloc(sizeof(TCB_t));	//malloc retorna um ponteiro do tipo (TCB_t*)
-	
-	
-	TCB_t* thread_leaving_cpu = thread_in_execution; //a thread que está saindo do estado de execução SEMPRE será a thread que está atualmente executando
-	
-	//agora precisamos que o escalonador escolha uma thread para ser executada, retire ela da fila de aptos 
-	//e nos retorne uma cópia do conteúdo da thread
-	//então vamos colocar esse retorno de escalonador() no ESPAÇO DE MEMÓRIA que alocamos, apontado por thread_arriving_CPU
-    *thread_arriving_CPU = escalonador(); //ESPAÇO DE MEMÓRIA apontado por thread_arriving_CPU RECEBE conteúdo retornado por escalonador()
-	thread_arriving_CPU->state = PROCST_EXEC; //modificamos o estado da thread_arriving_CPU para EXECUTANDO
-	
-	thread_leaving_cpu->state = new_state_thread_leaving_cpu; //modificamos o estado da thread_leaving_cpu para o estado passado como parâmetro
+    TCB_t thread_arriving_CPU;
+    TCB_t thread_leaving_cpu = thread_in_execution;
 
-	thread_in_execution = thread_arriving_CPU; //atualiza a variável global com a thread vinda do escalonador
+    thread_arriving_CPU = escalonador(); 
+    thread_arriving_CPU.state = PROCST_EXEC; 
+	
+    thread_leaving_cpu.state = new_state_thread_leaving_cpu; 
+
+    thread_in_execution = thread_arriving_CPU; 
 	
     if(new_state_thread_leaving_cpu == PROCST_APTO){
-        insere_na_fila_de_aptos(thread_leaving_cpu);
-		//inserimos a thread que está saindo na fila de aptos
-		
-		swapcontext(thread_leaving_cpu->context, thread_arriving_CPU->context);
-		//salvamos na thread que está saindo o contexto atual e trocamos para o contexto da thread que está entrando
-		//isto precisou ser feito pois quando a thread que está saindo voltar a executar, ela vai partir do contexto que foi salvo por último
-		//que é o contexto de agora, então precisamos garantir que daqui ela volte para sua execuçao normal
-		//na versão original, depois desse getcontext() tinha um setcontext() (após o if), então quando essa thread voltasse a executar,
-		//ela chamaria setcontext() e não executaria corretamente
+           insere_na_fila_de_aptos(&thread_leaving_cpu);
+	   swapcontext(&(thread_leaving_cpu.context), &(thread_arriving_CPU.context));
     }
     else if(new_state_thread_leaving_cpu == PROCST_TERMINO){
-        //thread_leaving_cpu = NULL;
-		free(thread_leaving_cpu); //queremos liberar o espaço ocupado por esta thread
-		setcontext(thread_arriving_CPU->context);
+	   setcontext(&(thread_arriving_CPU.context));
     }
     else if(new_state_thread_leaving_cpu == PROCST_BLOQ){
-		bloqueia(thread_leaving_cpu);
-		//inserimos a thread que está saindo na fila de aptos
-		swapcontext(thread_leaving_cpu->context, thread_arriving_CPU->context);
+	    bloqueia(&thread_leaving_cpu);
+	    swapcontext(&(thread_leaving_cpu.context), &(thread_arriving_CPU.context));
     }
 }
 
